a6578dead27b854e0ec719f44df040274950a536
Navrotskyi Aleksandr <navrotskyi.aleksandr@gmail.com>
2024-02-26 18:21:58 +0000

    chore: added CD Coalescing

--- apps/app-a/src/app/app.config.ts
+++ apps/app-a/src/app/app.config.ts
@@ -1,8 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideClientHydration(), provideRouter(appRoutes)],
+  providers: [
+    provideClientHydration(),
+    provideRouter(appRoutes),
+    provideZoneChangeDetection({
+      runCoalescing: true,
+      eventCoalescing: true,
+    }),
+  ],
 };
