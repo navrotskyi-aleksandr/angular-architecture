4d5cce5d61072e9e2e3cf2546d00878b15f25794
Navrotskyi Aleksandr <navrotskyi.aleksandr@gmail.com>
2024-04-25 18:57:38 +0100

    chore: added jest tresholds

--- apps/app-b/src/app/app.component.ts
+++ apps/app-b/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
   imports: [RouterModule],
   selector: 'navrik-root',
   templateUrl: './app.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'app-b';
--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,16 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  testMatch: ['**/+(*.)+(facade.spec).+(ts|js)?(x)'],
+  collectCoverage: true,
+  coverageReporters: ['text'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
